a4bcddf5e529db114f4133d4e18841033074380a
Jordi Serrano Berbel <jsberbel95@gmail.com>
2018-05-22 01:49:19 +0200

    Update Assert.h

--- SawassRayTracer/SawassRayTracer/Assert.h
+++ SawassRayTracer/SawassRayTracer/Assert.h
@@ -3,5 +3,5 @@
 #ifdef NDEBUG
 #define Assert(expression) ( (void) 0 )
 #else
-#define Assert(expression) if (!(condition)) { *(int*)0 = 0; }
+#define Assert(expression) if (!(expression)) { *(int*)0 = 0; }
 #endif
